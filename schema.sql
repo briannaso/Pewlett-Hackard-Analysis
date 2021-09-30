9070d8c21a46dd677851e8930876bcba964515ca
briannaso <sosa.brianna8@gmail.com>
2021-09-29 22:43:52 -0400

    schema.sql text saved

---
+++ schema.sql
@@ -0,0 +1,67 @@
+-- Create tables for Departments
+CREATE TABLE departments (
+     dept_no VARCHAR(4) NOT NULL,
+     dept_name VARCHAR(40) NOT NULL,
+     PRIMARY KEY (dept_no),
+     UNIQUE (dept_name)
+);
+
+-- Create tables for Employees
+CREATE TABLE employees (
+     emp_no INT NOT NULL,
+     birth_date DATE NOT NULL,
+     first_name VARCHAR NOT NULL,
+     last_name VARCHAR NOT NULL,
+     gender VARCHAR NOT NULL,
+     hire_date DATE NOT NULL,
+     PRIMARY KEY (emp_no)
+);
+
+-- Create table for dept_mgr
+CREATE TABLE dept_manager (
+	dept_no VARCHAR(4) NOT NULL,
+    emp_no INT NOT NULL,
+    from_date DATE NOT NULL,
+    to_date DATE NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+    PRIMARY KEY (emp_no, dept_no)
+);
+
+-- Create table for Salaries
+CREATE TABLE salaries (
+  emp_no INT NOT NULL,
+  salary INT NOT NULL,
+  from_date DATE NOT NULL,
+  to_date DATE NOT NULL,
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+  PRIMARY KEY (emp_no)
+);
+
+-- Create a table for Dep_Emp
+CREATE TABLE dept_emp (
+	dept_no VARCHAR(4) NOT NULL,
+	emp_no INT NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL, 
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	FOREIGN KEY (emp_no) REFERENCES salaries (emp_no),
+	PRIMARY KEY (dept_no, emp_no)
+	);
+	
+-- Create a table for Titles
+CREATE TABLE titles (
+	emp_no INT NOT NULL,
+	title VARCHAR(40) NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	FOREIGN KEY (emp_no) REFERENCES salaries (emp_no),
+	PRIMARY KEY (emp_no, title, from_date)
+	);
+	
+SELECT * from dept_emp ;
+
+
+
