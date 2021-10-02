4de9a55193a36f95c9881309c6e96d7afa2c59d4
briannaso <sosa.brianna8@gmail.com>
2021-10-01 22:42:05 -0400

    analysis outline for challenge, dept_info exported into csv

---
+++ analysis_readme.txt
@@ -0,0 +1,24 @@
+# Pewlett-Hackard-Analysis
+
+## Overview : Retirement Analysis
+The purpose of this analysis to to determine the number of employees at Pewlett Hackard that are eligable for retirement. We want to group the employees by department, title and select employees eligible for the mentorship program.
+
+## The Number of Retiring Employees by Title
+<ul>
+  <li>Coffee</li>
+  <li>Tea</li>
+  <li>Milk</li>
+  <li>Coffee</li>
+  <li>Tea</li>
+  <li>Milk</li>
+</ul>
+
+## The Employees Eligible for the Mentorship Program
+<ul>
+  <li>Coffee</li>
+  <li>Tea</li>
+  <li>Milk</li>
+  <li>Coffee</li>
+  <li>Tea</li>
+  <li>Milk</li>
+</ul>
--- queries.sql
+++ queries.sql
@@ -123,14 +123,53 @@ SELECT * FROM salaries
 ORDER BY to_date DESC;
 
 
--- create emp_info table to bring in the emp_no, first/last name & gender
-SELECT emp_no,
-    first_name,
-last_name,
-    gender
+-- create & join the emp_info table with dept_no, salaries, dept_emp, to get all employee info
+SELECT e.emp_no,
+    e.first_name,
+e.last_name,
+    e.gender,
+    s.salary,
+    de.to_date
 INTO emp_info
-FROM employees
-WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
-AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+FROM employees as e
+INNER JOIN salaries as s
+ON (e.emp_no = s.emp_no)
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+     AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+     AND (de.to_date = '9999-01-01');
 --check new table
 SELECT * FROM emp_info;
+
+
+-- List of managers per department
+SELECT  dm.dept_no,
+        d.dept_name,
+        dm.emp_no,
+        ce.last_name,
+        ce.first_name,
+        dm.from_date,
+        dm.to_date
+INTO manager_info
+FROM dept_manager AS dm
+    INNER JOIN departments AS d
+        ON (dm.dept_no = d.dept_no)
+    INNER JOIN current_emp AS ce
+        ON (dm.emp_no = ce.emp_no);
+
+Select * FROM manager_info;
+
+-- List of Department Retirees (7.3.5)
+SELECT ce.emp_no,
+ce.first_name,
+ce.last_name,
+d.dept_name
+-- INTO dept_info
+FROM current_emp as ce
+INNER JOIN dept_emp AS de
+ON (ce.emp_no = de.emp_no)
+INNER JOIN departments AS d
+ON (de.dept_no = d.dept_no);
+-- Check Table
+SELECT * FROM dept_info;
