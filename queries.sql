c802cc3dc8c7a9eab3004f32c991fb66a84d7c82
briannaso <sosa.brianna8@gmail.com>
2021-10-01 22:01:24 -0400

    used group by and order by functions to organize employees by dept

--- queries.sql
+++ queries.sql
@@ -109,3 +109,15 @@ WHERE de.to_date = ('9999-01-01');
 
 SELECT * FROM current_emp;
 
+-- Using GROUP BY/ ORDER BY: Employee count by department number
+SELECT COUNT(ce.emp_no), de.dept_no
+FROM current_emp as ce
+LEFT JOIN dept_no as de
+ON ce.emp_no = de.emp_no
+GROUP BY de.dept_no
+ORDER BY de.dept_no;
+
+
+-- checking the salaries table
+SELECT * FROM salaries
+ORDER BY to_date DESC;
